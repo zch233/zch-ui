f218159620366d6ba884b8ac57e556b919d6a5f7
zhangchao <529743595@qq.com>
2019-11-08 16:51:47 +0800

    delete ignore

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12514,7 +12514,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61915" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62869" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
