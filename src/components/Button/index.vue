c56148bb9c33e22cd4830507b51eaf368bf6d55d
zhangchao <529743595@qq.com>
2019-11-08 20:47:17 +0800

    Add focus active css

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -1,13 +1,47 @@
 <template>
-  <button>aaaa</button>
+  <button class="zch-button">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+$fontColor: #606266;
+.zch-button {
+  $bgColor: #fff;
+  $borderColor: #ddd;
+  $fontSize: 14px;
+  border: 1px solid $borderColor;
+  background-color: $bgColor;
+  padding: .86em 1.44em;
+  font-size: $fontSize;
+  line-height: 1;
+  border-radius: 4px;
+  white-space: nowrap;
+  transition: all .2s;
+  color: $fontColor;
+  cursor: pointer;
+  &:hover {
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
+  }
+  &:active {
+    color: #3a8ee6;
+    border-color: #3a8ee6;
+    outline: none;
+  }
+  &:focus {
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
+  }
+}
 </style>
