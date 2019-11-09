f362c5b6cd3f98eb897378711869f1c9b820c441
zhangchao <529743595@qq.com>
2019-11-09 22:58:21 +0800

    Modify travis config

--- test/button.test.js
+++ test/button.test.js
@@ -1,6 +1,6 @@
 const expect = chai.expect;
 import Vue from 'vue'
-import zchButton from '../src/components/button'
+import zchButton from '../src/components/button/index.vue'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
