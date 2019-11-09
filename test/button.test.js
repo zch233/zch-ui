3bca33a8d3c9b7819ed033e3490f00b4d8da2c61
zhangchao <529743595@qq.com>
2019-11-09 20:57:48 +0800

    重写测试用例

--- test/button.test.js
+++ test/button.test.js
@@ -1,12 +1,91 @@
 const expect = chai.expect;
 import Vue from 'vue'
-import Button from '../src/components/button'
+import zchButton from '../src/components/button'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
-describe('Button', () => {
+describe('zchButton', () => {
   it('存在', () => {
-    expect(Button).to.be.exist
+    expect(zchButton).to.be.exist
+  })
+  it('添加按钮 icon ', () => {
+    const Constructor = Vue.extend(zchButton)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting'
+      }
+    })
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('按钮 loading', () => {
+    const Constructor = Vue.extend(zchButton)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true
+      }
+    })
+    vm.$mount()
+    const useElement = vm.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-loading')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('按钮 right iconPosition', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(zchButton)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'right',
+        iconPosition: 'right'
+      }
+    })
+    vm.$mount(div)
+    const svgElement = vm.$el.querySelector('svg')
+    const { order } = window.getComputedStyle(svgElement)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('按钮默认 iconPosition', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(zchButton)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'left'
+      }
+    })
+    vm.$mount(div)
+    const svgElement = vm.$el.querySelector('svg')
+    const { order } = window.getComputedStyle(svgElement)
+    expect(order).to.eq('0')
+    vm.$el.remove()
+    vm.$destroy()
+  })
+  it('按钮 disabled', () => {
+    const Constructor = Vue.extend(zchButton)
+    const vm = new Constructor({
+      propsData: {
+        disabled: true
+      }
+    })
+    vm.$mount()
+    const button = vm.$el
+    const classList = button.classList
+    const hasClassDisabled = classList.contains('disabled')
+    const hasAttributeDisabled = button.hasAttribute('disabled')
+    expect(hasClassDisabled).to.eq(true)
+    expect(hasAttributeDisabled).to.eq(true)
+    vm.$el.remove()
+    vm.$destroy()
   })
 })
