f76c79cfbc19473c179cdd83d8e6f2a711db323e
zhangchao <529743595@qq.com>
2019-11-09 22:56:13 +0800

    Modify travis config

---
+++ index.js
@@ -0,0 +1,5 @@
+import zchButton from './src/components/button/index'
+import zchButtonGroup from './src/components/button-group/index'
+import zchIcon from './src/components/icon/index'
+
+export { zchButton, zchButtonGroup, zchIcon }
