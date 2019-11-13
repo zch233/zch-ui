c7f0faffc5ca7a1e1c68b41307a5013cb934476b
zhangchao <529743595@qq.com>
2019-11-13 22:56:37 +0800

    test

--- test/button.test.js
+++ test/button.test.js
@@ -1,6 +1,6 @@
 const expect = chai.expect;
 import Vue from 'vue'
-import zchButton from '../src/index'
+import zchButton from '../src/aa/index'
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
