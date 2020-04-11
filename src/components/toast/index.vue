ed8929624e756a6d78ea179dec42aad66ca4f89f
zhangchao <529743595@qq.com>
2020-04-11 22:47:45 +0800

    Add Toast $emit("click")

--- src/components/toast/index.vue
+++ src/components/toast/index.vue
@@ -42,6 +42,7 @@ export default {
   methods: {
     close () {
       this.beforeClose && this.beforeClose()
+      this.$emit('click', this)
       this.$el.remove()
       this.$destroy()
     },
