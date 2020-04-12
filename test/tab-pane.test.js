dbe44677ab7eb9fa44ad206bed6411bc388c1f22
zhangchao <529743595@qq.com>
2020-04-12 11:29:41 +0800

    Add TabPane Disabled Test

--- test/tab-pane.test.js
+++ test/tab-pane.test.js
@@ -11,4 +11,33 @@ describe('ZchTabPane', () => {
   it('存在', () => {
     expect(ZchTabPane).to.be.exist
   })
+  it('接受 disabled ', (done) => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    Vue.component('zch-tab', ZchTab)
+    Vue.component('zch-tab-pane', ZchTabPane)
+    div.innerHTML = `
+      <zch-tab v-model="activeName">
+        <zch-tab-pane name="title1"111</zch-tab-pane>
+        <zch-tab-pane disabled name="title2">2222</zch-tab-pane>
+        <zch-tab-pane name="title3">3333</zch-tab-pane>
+      </zch-tab>
+    `
+    const vm = new Vue({
+      el: div,
+      data () {
+        return {
+          activeName: 'title1',
+        }
+      }
+    })
+    setTimeout(() => {
+      const navElement = vm.$el.querySelector(`.zchTabNav[data-name="title2"]`)
+      expect(navElement.classList.contains('zchTabNav-disabled')).to.eq(true)
+      done()
+      div.remove()
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
 })
