f515a4fe3d7c4f0bd4cd691d928f525bb49d5d37
zhangchao <529743595@qq.com>
2020-04-19 22:26:03 +0800

    Add Accordion

--- test/collapse.test.js
+++ test/collapse.test.js
@@ -44,4 +44,46 @@ describe('zchCollapse', () => {
       vm.$destroy()
     })
   })
+  it('接受 accordion ', (done) => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    Vue.component('zch-collapse', zchCollapse)
+    Vue.component('zch-collapse-item', zchCollapseItem)
+    div.innerHTML = `
+      <zch-collapse v-model="activeName" accordion>
+        <zch-collapse-item title="title1" name="title1">
+          内容111
+        </zch-collapse-item>
+        <zch-collapse-item title="title2" name="title2">
+          内容222
+        </zch-collapse-item>
+        <zch-collapse-item title="title3" name="title3">
+          内容333
+        </zch-collapse-item>
+        <zch-collapse-item title="title4" name="title4">
+          内容444
+        </zch-collapse-item>
+      </zch-collapse>
+    `
+    const activeName = 'title2'
+    const vm = new Vue({
+      el: div,
+      data () {
+        return {
+          activeName,
+        }
+      }
+    })
+    const titleElements = vm.$el.querySelectorAll('.zch-collapse-item-title');
+    [...titleElements].map(element => element.click())
+    setTimeout(() => {
+      const contentElements = vm.$el.querySelectorAll('.zch-collapse-item-content')
+      const activeContents = [...contentElements].filter(v => v.classList.contains('active'))
+      expect(activeContents.length).to.eq(1)
+      done()
+      div.remove()
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
 })
