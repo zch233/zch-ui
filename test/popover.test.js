c221749f7a7a2f765de355d25d29fb1233c891d7
zhangchao <529743595@qq.com>
2020-04-19 15:52:05 +0800

    Test Popover Position

---
+++ test/popover.test.js
@@ -0,0 +1,35 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import zchPopover from '../src/components/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('zchPopover', () => {
+  const Constructor = Vue.extend(zchPopover)
+  it('存在', () => {
+    expect(zchPopover).to.be.exist
+  })
+  it('接受 position', (done) => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    Vue.component('zch-popover', zchPopover)
+    div.innerHTML = `
+      <zch-popover position="left" ref="test">
+        <template slot="content">这是一段浮层</template>
+        <button>left</button>
+      </zch-popover>
+    `
+    const vm = new Vue({
+      el: div,
+    })
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      const popoverElement = vm.$refs.test.$refs.popoverContent
+      expect(popoverElement.classList.contains('zchPopover-popoverWrapper-left')).to.eq(true)
+      expect(popoverElement.querySelector('.zchPopover-popover').classList.contains('zchPopover-popover-left-arrow')).to.eq(true)
+      done()
+    })
+  })
+  xit('测试 trigger', () => {})
+})
