7e524763df60fdbcd882992e099bf8f58864be4a
zhangchao <529743595@qq.com>
2020-04-22 11:11:46 +0800

    Add Head Icon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,8 @@
 module.exports = {
   base: '/zch-ui/',
   title: 'Zch - 网站快速成型工具',
-  description: 'Zch，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库'
+  description: 'Zch，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ],
 }
