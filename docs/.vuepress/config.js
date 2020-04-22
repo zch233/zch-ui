e472ec2ec5209a6569e7153b96fd24357abc12bc
zhangchao <529743595@qq.com>
2020-04-22 15:46:02 +0800

    Add Basic Menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,10 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }]
   ],
+  plugins: ['@vuepress/back-to-top', '@vuepress/nprogress', '@vuepress/active-header-links'],
   themeConfig: {
     logo: '/logo.png',
+    displayAllHeaders: true,
     nav: [
       { text: '首页', link: '/' },
       { text: '组件', link: '/components/' },
@@ -14,8 +16,34 @@ module.exports = {
         text: '选择语言',
         items: [ { text: '简体中文', link: '?Chinese' }, { text: 'English', link: '?English' } ]
       },
-      { text: '预览', link: 'https://zch233.github.io/zch-ui/', target:'_blank' },
+      { text: 'NPM', link: 'https://www.npmjs.com/package/@zch233/zch-ui', target:'_blank' },
       { text: 'GitHub', link: 'https://github.com/zch233/zch-ui', target:'_blank' },
     ],
+    sidebar: {
+      '/components/': [
+        {
+          title: '通用',
+          collapsable: false,
+          children: [
+            { title: '按钮', path: '/components/basic/button/' }
+          ]
+        },
+        {
+          title: '数据录入',
+          collapsable: false,
+          children: [ /* ... */ ]
+        },
+        {
+          title: '数据展示',
+          collapsable: false,
+          children: [ /* ... */ ]
+        },
+        {
+          title: '反馈',
+          collapsable: false,
+          children: [ /* ... */ ]
+        },
+      ],
+    }
   }
 }
