59cf1b2cd0a3752ab9591184494ccf93f23ac3b7
zhangchao <529743595@qq.com>
2020-04-23 16:29:17 +0800

    Update Npm Package

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-import zchButton from './src/components/button/index'
-import zchButtonGroup from './src/components/button-group/index'
-import zchIcon from './src/components/icon/index'
+import zchButton from './components/button'
+import zchButtonGroup from './components/button-group'
+import zchIcon from './components/icon'
+import zchInput from './components/input'
+import zchRow from './components/row'
+import zchCol from './components/col'
+import zchLayout from './components/layout'
+import zchHeader from './components/header'
+import zchContent from './components/content'
+import zchSide from './components/side'
+import zchFooter from './components/footer'
+import zchToast from './components/toast/main.js'
+import zchTab from './components/tabs/tab'
+import zchTabPane from './components/tabs/tab-pane'
+import zchPopover from './components/popover'
+import zchCollapse from './components/collapse/collapse'
+import zchCollapseItem from './components/collapse/collapse-item'
+import zchTag from './components/tag'
 
-export { zchButton, zchButtonGroup, zchIcon }
+
+export {
+  zchButton,
+  zchButtonGroup,
+  zchIcon,
+  zchInput,
+  zchRow,
+  zchCol,
+  zchLayout,
+  zchHeader,
+  zchContent,
+  zchSide,
+  zchFooter,
+  zchToast,
+  zchTab,
+  zchTabPane,
+  zchPopover,
+  zchCollapse,
+  zchCollapseItem,
+  zchTag,
+}
