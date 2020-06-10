3f994317e99df8e374ea5b1352acb8daa60497d9
zhangchao <529743595@qq.com>
2020-06-10 17:46:03 +0800

    Add Radio Test, Modle

--- tests/unit/radio.spec.js
+++ tests/unit/radio.spec.js
@@ -9,4 +9,23 @@ describe('ZchRadio', () => {
   it('存在', () => {
     expect(ZchRadio).to.be.exist
   })
+  it('接受 v-model ', async () => {
+    const radio = '2'
+    const Component = {
+      components: { ZchRadio },
+      data () {
+        return { radio }
+      },
+      template: `
+        <div>
+          <zch-radio v-model="radio" label="1">选项一</zch-radio>
+          <zch-radio v-model="radio" label="2">选项二</zch-radio>
+          <zch-radio v-model="radio" label="3">选项三</zch-radio>
+        </div>
+      `,
+    }
+    const wrapper = mount(Component)
+    await wrapper.vm.$nextTick()
+    expect(wrapper.findAll('.zchRadio').at(1).classes().includes('checked')).to.eq(true)
+  })
 })
